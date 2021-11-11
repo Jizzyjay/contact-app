be830159bc8c561a104f30dba9630d90e6b9606f
Ekunola John Oluwatosin <ekunolajohn@gmail.com>
2021-11-11 15:36:45 +0100

    contact app

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../images/img.jpeg";
+
+const ContactDetail = (props) => {
+    const {name, email } = props.location.state.contact;
+    
+    return (
+        <div className="main">
+            <div className="ui card centered">
+                <div className="image">
+                    <img src={user} alt="user" />
+                </div>
+                <div className="content">
+                    <div className="header">John</div>
+                    <div className="description">jizzyjay@gmail.com</div>
+                </div>
+            </div>
+            <div className="center-div">
+                <Link to="/">
+                    <button classname="ui button blue center">Back to Contact List</button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default ContactDetail;
